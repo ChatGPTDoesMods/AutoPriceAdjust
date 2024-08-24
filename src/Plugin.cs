b256b98a52dc7ddd29345a0a23cba949b69a33e4
Dylan Ryan <drryan@ncsu.edu>
2024-08-24 00:57:56 -0400

    add source so people can leave feedback

---
+++ src/Plugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace AutoPriceAdjust;
+
+[BepInPlugin("AutoPriceAdjust", "Auto Price Adjust", "1.0.0")]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+
+    internal static HarmonyLib.Harmony Harmony;
+
+    private void Awake()
+    {
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin AutoPriceAdjust is loaded!");
+        Harmony = new("mod.supermarkettogether.autopriceadjust");
+        Harmony.PatchAll();
+    }
+}
